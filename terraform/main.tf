35d362f7863731a035d32fdb0738f1ffb58d17c0
tamakiii <tamakiii@users.noreply.github.com>
2026-01-03 17:03:01 +0900

    ⏺ Done. I've added the S3 backend configuration. Now run:
      terraform init
    
      Terraform will detect the backend change and initialize the S3 backend. If you have existing local state, it will ask if you want to migrate it to S3.

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "6.27.0"
     }
   }
+
+  backend "s3" {
+    bucket = "terraform.s3.tamakiii.com"
+    key    = "blog/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
 }
 
 provider "aws" {
